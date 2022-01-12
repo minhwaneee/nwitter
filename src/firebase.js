48097233bfc380b8ad43a4787a283f9931bdce1e
Steady <coco11@naver.com>
2022-01-13 00:41:16 +0900

    Chapter 2 commit

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/compat/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCAEmxj5Vm8NRGybDETLxuC5LsPZQ_x6h8",
+  authDomain: "nwitter-de96e.firebaseapp.com",
+  projectId: "nwitter-de96e",
+  storageBucket: "nwitter-de96e.appspot.com",
+  messagingSenderId: "150582986605",
+  appId: "1:150582986605:web:a53101b17038afb4ae3c64"
+};
+
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
